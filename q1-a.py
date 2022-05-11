6eb10f2f687ee53241b86c209e5aa4897d5dd59f
Samarasuleiman <samarasuleiman2438@gmail.com>
2022-05-11 07:01:23 -0300

    Add files via upload

---
+++ q1-a.py
@@ -0,0 +1,8 @@
+l=['samara','ali','karam','sara','rana']
+sname=input('inter student name:')
+if sname in l:
+    print(sname)
+    print("the user is graduted")
+else:
+    print("the user is not gradutd")
+    
